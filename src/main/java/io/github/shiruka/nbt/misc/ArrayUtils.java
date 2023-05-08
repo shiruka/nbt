34e92fdac880f15631180547448a3be6628fa27f
Hasan Demirtaş <utsukushihito@outlook.com>
2023-05-08 22:02:38 +0300

    Update ArrayUtils.java

--- src/main/java/io/github/shiruka/nbt/misc/ArrayUtils.java
+++ src/main/java/io/github/shiruka/nbt/misc/ArrayUtils.java
@@ -4,15 +4,34 @@ import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * A class that contains utility methods for arrays.
+ */
 @UtilityClass
 public class ArrayUtils {
 
+  /**
+   * an empty byte array as object.
+   */
   private static final Byte[] EMPTY_BYTE_OBJECT_ARRAY = new Byte[0];
 
+  /**
+   * an empty integer array as object.
+   */
   private static final Integer[] EMPTY_INTEGER_OBJECT_ARRAY = new Integer[0];
 
+  /**
+   * an empty long array as object.
+   */
   private static final Long[] EMPTY_LONG_OBJECT_ARRAY = new Long[0];
 
+  /**
+   * converts the primitive array into object array.
+   *
+   * @param array the array to convert.
+   *
+   * @return object array.
+   */
   @Contract("null -> null; !null -> !null")
   public Byte@Nullable[] toObject(final byte@Nullable[] array) {
     if (array == null) {
@@ -28,6 +47,13 @@ public class ArrayUtils {
     return result;
   }
 
+  /**
+   * converts the primitive array into object array.
+   *
+   * @param array the array to convert.
+   *
+   * @return object array.
+   */
   @Contract("null -> null; !null -> !null")
   public Integer@Nullable[] toObject(final int@Nullable[] array) {
     if (array == null) {
@@ -43,6 +69,13 @@ public class ArrayUtils {
     return result;
   }
 
+  /**
+   * converts the primitive array into object array.
+   *
+   * @param array the array to convert.
+   *
+   * @return object array.
+   */
   @Contract("null -> null; !null -> !null")
   public Long@Nullable[] toObject(final long@Nullable[] array) {
     if (array == null) {
